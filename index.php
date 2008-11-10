36efb845ed61306846b5a0a1e5310a471c2566da
Trevor Joynson (trevorj) <github@skywww.net>
2008-11-10 17:56:15 +0000

    Fix stupid newlines
    git-svn-id: https://svn.skywww.net/repos/skywww/spg/trunk@47 18765250-b91b-0410-8a00-f77a687b104c

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
 /*
-    spg - Simple (PHP|Photo) Gallery)                                             ~trevorj <[trevorjoynson@gmail.com]>
+    spg - Simple (PHP|Photo) Gallery)
+    ~trevorj <[trevorjoynson@gmail.com]>
 
     Copyright 2006 Trevor Joynson
 
     This file is part of spg.
 
     spg is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by          the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.                                           
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
     spg is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     
     You should have received a copy of the GNU General Public License
-    along with spg; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    along with spg; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin St, Fifth Floor,
+    Boston, MA  02110-1301  USA
 */
 
 // Define this so our children won't cry on contact
